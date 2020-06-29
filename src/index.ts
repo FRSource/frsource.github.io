feaf71b68c6668a62658c0439d4fa60948e58475
Jakub Freisler <jakubfreisler@phrase.com>
2020-06-29 03:21:01 +0200

    feat: first desktop version

--- src/index.ts
+++ src/index.ts
@@ -10,14 +10,22 @@ if (!matchMedia('(prefers-reduced-motion: reduce)').matches) {
         canvas.className = 'icon-logomark icon-logomark--canvas';
         logo.parentElement.appendChild(canvas);
 
+        let canvasSize = {w: window.innerWidth - 30, h: 86};
+        let imgSize = {w: 84, h: 72};
+        
+        if (matchMedia('(min-width:920px)').matches) {
+            canvasSize = {w: window.innerWidth/2 - 30, h: window.innerHeight * .45 + 40};
+            imgSize = {w: window.innerHeight / 2 * 1.666667, h: window.innerHeight * .45};
+        }
+
         paths.push(logoType, logoTypeText);
         paths.forEach(path => path.style.display = 'none');
         new module.LogoCtrl(
             logo,
             canvas,
             paths,
-            {w: window.innerWidth - 30, h: 86},
-            {w: 84, h: 72}
+            canvasSize,
+            imgSize
         ).init();
     });
 }
