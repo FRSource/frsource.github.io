a07576a453bd448a32f9344c93a5ef63f43b2ae1
Jakub Freisler <jakub@frsource.org>
2022-12-09 03:36:43 +0100

    chore: move to monorepo
    Signed-off-by: Jakub Freisler <jakub@frsource.org>

--- packages/index/build/faviconsInjector.js
+++ packages/index/build/faviconsInjector.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
-const faviconsHTML = fs.readFileSync('./public/html_code.html').toString();
-module.exports = r => 
+const faviconsHTML = fs.readFileSync('../../public/html_code.html').toString();
+module.exports = r =>
         faviconsHTML
             .replace(/[\n\r]/g, '') // remove new lines
             .replace(/rel="icon"/g, 'rel="alternate icon"') // make png icons "alternate" to set priority on the SVG icon
